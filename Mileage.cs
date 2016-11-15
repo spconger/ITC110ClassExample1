800c4ce70f1c5412b4ac99ea29657e014bda7cfb
Steve Conger <steve@spconger.com>
2016-11-15 12:11:20 -0800

    CommitProgramFiles

---
+++ Mileage.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ObjectExample1
+{
+    public class Mileage
+    {
+        //fields--class level variable
+        //that describes the class
+        private double miles;
+        private double gallons;
+        private double pricePerGallon;
+
+        //constructor
+        public Mileage()
+        {
+            Miles = 0;
+            Gallons = 0;
+            PricePerGallon = 0;
+
+        }
+
+        public Mileage(double totalMiles, double totalGallons, double gallonPrice)
+        {
+            Miles = totalMiles;
+            Gallons = totalGallons;
+            PricePerGallon = gallonPrice;
+        }
+
+
+        //properties expose private
+        //fields to other classes
+        //set lets the variable be changed
+        //get returns the value of the variable
+        public double Miles
+        {
+            set { miles = value; }
+            get { return miles; }
+        }
+
+        public double Gallons
+        {
+            get
+            {
+                return gallons;
+            }
+
+            set
+            {
+                gallons = value;
+            }
+        }
+
+        public double PricePerGallon
+        {
+            get
+            {
+                return pricePerGallon;
+            }
+
+            set
+            {
+                pricePerGallon = value;
+            }
+        }
+
+        //public methods
+        public double GetMilesPerGallon()
+        {
+            return Miles / Gallons;
+        }
+
+        public double PricePerMile()
+        {
+            double total = PricePerGallon * Gallons;
+            return total / Miles;
+        }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ObjectExample1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Mileage m = new Mileage();
+            double miles = 300;
+            double gallons = 12;
+            double pricePerGallon = 2.50;
+
+            Mileage m = new Mileage(miles, gallons, pricePerGallon);
+            
+            
+
+            Console.WriteLine("Your Miles per gallon: {0}", m.GetMilesPerGallon());
+            Console.WriteLine("It cost you {0} per mile", m.PricePerMile());
+
+            Console.WriteLine("Press any key to exit");
+            Console.ReadKey();
+
+        }
+    }
+}
